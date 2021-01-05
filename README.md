266840c4baac8f65d8b55cec6bdd5a73568605e8
Anderson Barros <anderson.barros@selcorp.com.br>
2021-01-05 13:49:52 -0300

    feat: Insert files and update readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Estudo do Curso JS com TDD
+
+Player de exemplo do curso JS com TDD
---
+++ src/Spotify.js
@@ -0,0 +1,8 @@
+import SpotifyExample from "spotify-example";
+
+const spotify = new SpotifyExample({
+  token:
+    "BQALqTsjYqIXtLNUom9ETZc1E9iph30igV8GySCIGqLkrtQR-unagwWiqryhnLY-cnjInpTj2hMhefM4A_S0t9pfFf2qsdjVKXvgucykeeXH-mxIwH19gnbIxLk41kwGBKOr5nfEFeyPoQZW72U",
+});
+
+export default spotify;
